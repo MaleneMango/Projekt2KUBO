6363f7941f3c21375bbb4af814689ec4c2b1bb82
Malene Ulm <32540888+MaleneMango@users.noreply.github.com>
2018-04-08 12:36:45 +0200

    Testing JS

--- script.js
+++ script.js
@@ -2,3 +2,64 @@ window.onload = function() {
     document.getElementById("showlessonplans").checked = true;
     document.getElementById("showposts").checked = true;
 }
+
+
+// show/hide lessonplans
+
+function showLessonplans() {
+    var l1 = document.getElementById('l1');
+    if (l1.style.display === 'none') {
+        l1.style.display = 'block';
+    } else {
+        l1.style.display = 'none';
+    }
+    var l2 = document.getElementById('l2');
+    if (l2.style.display === 'none') {
+        l2.style.display = 'block';
+    } else {
+        l2.style.display = 'none';
+    }
+    var l3 = document.getElementById('l3');
+    if (l3.style.display === 'none') {
+        l3.style.display = 'block';
+    } else {
+        l3.style.display = 'none';
+    }
+    var l4 = document.getElementById('l4');
+    if (l4.style.display === 'none') {
+        l4.style.display = 'block';
+    } else {
+        l4.style.display = 'none';
+    }
+    var l5 = document.getElementById('l5');
+    if (l5.style.display === 'none') {
+        l5.style.display = 'block';
+    } else {
+        l5.style.display = 'none';
+    }
+    var l6 = document.getElementById('l6');
+    if (l6.style.display === 'none') {
+        l6.style.display = 'block';
+    } else {
+        l6.style.display = 'none';
+    }
+    var l7 = document.getElementById('l7');
+    if (l7.style.display === 'none') {
+        l7.style.display = 'block';
+    } else {
+        l7.style.display = 'none';
+    }
+    var l8 = document.getElementById('l8');
+    if (l8.style.display === 'none') {
+        l8.style.display = 'block';
+    } else {
+        l8.style.display = 'none';
+    }
+    var l9 = document.getElementById('l9');
+    if (l9.style.display === 'none') {
+        l9.style.display = 'block';
+    } else {
+        l9.style.display = 'none';
+    }
+
+}
