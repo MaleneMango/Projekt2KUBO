d5de449f6f0a66f18766c1176ee7e6687a890c23
Malene <32540888+MaleneMango@users.noreply.github.com>
2018-04-16 12:24:51 +0200

    localStorage commentsection

---
+++ script2.js
@@ -0,0 +1,34 @@
+var form = document.querySelector('form');
+var ul = document.querySelector('ul');
+var button = document.querySelector('button');
+var input = document.getElementById('item');
+let commentsArray = localStorage.getItem('comments') ? JSON.parse(localStorage.getItem('comments')) : [];
+
+localStorage.setItem('comments', JSON.stringify(commentsArray));
+var data = JSON.parse(localStorage.getItem('comments'));
+
+var liMaker = (text) => {
+  var li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  commentsArray.push(input.value);
+  localStorage.setItem('comments', JSON.stringify(commentsArray));
+  liMaker(input.value);
+  input.value = "";
+});
+
+data.forEach(item => {
+  liMaker(item);
+});
+
+button.addEventListener('click', function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+});
